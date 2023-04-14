084b33dcf7504115866c4bf3559f33f2428f772e
miroslav-zarenkov <miroslav.zarenkov@gmail.com>
2023-04-14 20:44:08 +0400

    feat: done count-cats and carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -6,22 +6,35 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (
+    typeof sampleActivity !== "string" ||
+    +sampleActivity > 15 ||
+    +sampleActivity <= 0 ||
+    isNaN(+sampleActivity)
+  )
+    return false;
+
+  const MODERN_ACTIVITY = 15;
+  const HALF_LIFE_PERIOD = 5730;
+  lnTwo = 0.693;
+  let k = lnTwo / HALF_LIFE_PERIOD;
+  let t = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / k);
+
+  return t;
 }
 
 module.exports = {
-  dateSample
+  dateSample,
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,9 +1,9 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,11 +14,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(backyard) {
+  let number = 0;
+  let backyardLength = backyard.length;
+  if (backyardLength === 0) return number;
+  let elementLength = backyard[0].length;
+
+  for (let i = 0; i < backyardLength; i++) {
+    for (let j = 0; j < elementLength; j++) {
+      if (backyard[i][j] === "^^") number++;
+    }
+  }
+  return number;
 }
 
 module.exports = {
-  countCats
+  countCats,
 };
