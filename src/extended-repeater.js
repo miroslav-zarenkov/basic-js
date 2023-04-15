0e6d0f7dcf0437bd438ac4bd4549c25e82e6446a
miroslav-zarenkov <miroslav.zarenkov@gmail.com>
2023-04-15 23:37:29 +0400

    feat: done extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,54 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  str = str + "";
+  let finalString = "";
+  let repeatTimesVar = options["repeatTimes"] || 1;
+  let separatorVar = options["separator"] || "+";
+  let additionVar = options["addition"];
+  let additionRepeatTimes = options["additionRepeatTimes"] || 1;
+  let additionSeparator = options["additionSeparator"] || "|";
+
+  if (additionVar !== undefined) {
+    if (additionRepeatTimes) {
+      for (let i = 0; i < additionRepeatTimes; i++) {
+        if (i === additionRepeatTimes - 1) {
+          str += `${additionVar}`;
+        } else {
+          str += `${additionVar}${additionSeparator}`;
+        }
+      }
+    } else {
+      str += `${additionVar}`;
+    }
+  }
+
+  for (let i = 0; i < repeatTimesVar; i++) {
+    if (i === repeatTimesVar - 1) {
+      finalString += `${str}`;
+    } else {
+      finalString += `${str}${separatorVar}`;
+    }
+  }
+
+  return finalString;
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
